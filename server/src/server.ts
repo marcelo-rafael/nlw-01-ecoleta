b6a5049a21371049b232cbe1c4b21311f358e262
marcelo-rafael <marcelo.rafael.goncalves@gmail.com>
2020-09-26 12:09:21 -0300

    conceitos

---
+++ server/src/server.ts
@@ -0,0 +1,38 @@
+import express, { response } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const users = ['Marcelo', 'Maicon', 'Marlon', 'Marcio'];
+
+app.get('/users', (request, response) => {
+  const search = String(request.query.search);
+
+  const filteredUsers = search
+    ? users.filter((user) => user.includes(search))
+    : users;
+
+  return response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+
+  return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+  const data = request.body;
+
+  const user = {
+    name: data.nome,
+    email: data.email,
+  };
+
+  return response.json(user);
+});
+
+app.listen(3333);
