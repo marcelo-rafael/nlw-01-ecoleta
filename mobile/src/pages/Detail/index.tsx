379533cac5a53cc5926f992a34dc7344c816fc66
marcelo-rafael <marcelo.rafael.goncalves@gmail.com>
2020-09-29 11:09:21 -0300

    configurar navegaçao

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,73 +1,82 @@
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 32,
-    paddingTop: 20,
-  },
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 
-  pointImage: {
-    width: '100%',
-    height: 120,
-    resizeMode: 'cover',
-    borderRadius: 10,
-    marginTop: 32,
-  },
+const Detail = () => {
+  return <View />;
+};
 
-  pointName: {
-    color: '#322153',
-    fontSize: 28,
-    fontFamily: 'Ubuntu_700Bold',
-    marginTop: 24,
-  },
+export default Detail;
 
-  pointItems: {
-    fontFamily: 'Roboto_400Regular',
-    fontSize: 16,
-    lineHeight: 24,
-    marginTop: 8,
-    color: '#6C6C80'
-  },
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     padding: 32,
+//     paddingTop: 20,
+//   },
 
-  address: {
-    marginTop: 32,
-  },
-  
-  addressTitle: {
-    color: '#322153',
-    fontFamily: 'Roboto_500Medium',
-    fontSize: 16,
-  },
+//   pointImage: {
+//     width: '100%',
+//     height: 120,
+//     resizeMode: 'cover',
+//     borderRadius: 10,
+//     marginTop: 32,
+//   },
 
-  addressContent: {
-    fontFamily: 'Roboto_400Regular',
-    lineHeight: 24,
-    marginTop: 8,
-    color: '#6C6C80'
-  },
+//   pointName: {
+//     color: '#322153',
+//     fontSize: 28,
+//     fontFamily: 'Ubuntu_700Bold',
+//     marginTop: 24,
+//   },
 
-  footer: {
-    borderTopWidth: StyleSheet.hairlineWidth,
-    borderColor: '#999',
-    paddingVertical: 20,
-    paddingHorizontal: 32,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  
-  button: {
-    width: '48%',
-    backgroundColor: '#34CB79',
-    borderRadius: 10,
-    height: 50,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
+//   pointItems: {
+//     fontFamily: 'Roboto_400Regular',
+//     fontSize: 16,
+//     lineHeight: 24,
+//     marginTop: 8,
+//     color: '#6C6C80',
+//   },
 
-  buttonText: {
-    marginLeft: 8,
-    color: '#FFF',
-    fontSize: 16,
-    fontFamily: 'Roboto_500Medium',
-  },
-});
+//   address: {
+//     marginTop: 32,
+//   },
+
+//   addressTitle: {
+//     color: '#322153',
+//     fontFamily: 'Roboto_500Medium',
+//     fontSize: 16,
+//   },
+
+//   addressContent: {
+//     fontFamily: 'Roboto_400Regular',
+//     lineHeight: 24,
+//     marginTop: 8,
+//     color: '#6C6C80',
+//   },
+
+//   footer: {
+//     borderTopWidth: StyleSheet.hairlineWidth,
+//     borderColor: '#999',
+//     paddingVertical: 20,
+//     paddingHorizontal: 32,
+//     flexDirection: 'row',
+//     justifyContent: 'space-between',
+//   },
+
+//   button: {
+//     width: '48%',
+//     backgroundColor: '#34CB79',
+//     borderRadius: 10,
+//     height: 50,
+//     flexDirection: 'row',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+
+//   buttonText: {
+//     marginLeft: 8,
+//     color: '#FFF',
+//     fontSize: 16,
+//     fontFamily: 'Roboto_500Medium',
+//   },
+// });
